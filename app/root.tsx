7c9eb3d329b58c5fb516938eaad6e1db49ddc60e
Igor Pedrosa <ilap1036@gmail.com>
2023-05-07 11:55:41 -0300

    feat: styling
    Signed-off-by: Igor Pedrosa <ilap1036@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,26 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import { LiveReload, Outlet, Links } from "@remix-run/react";
+import globalSyle from "~/styles/global.css";
+import globalLargeStyles from "~/styles/global-large.css";
+import globalMediumStyles from "~/styles/global-medium.css";
+
+import type { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: globalSyle,
+  },
+  {
+    rel: "stylesheet",
+    href: globalMediumStyles,
+    media: "print, (min-width: 640px)",
+  },
+  {
+    rel: "stylesheet",
+    href: globalLargeStyles,
+    media: "screen and (min-width: 1024px)",
+  },
+];
 
 export default function App() {
   return (
@@ -6,6 +28,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
